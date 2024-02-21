1f8b81b203872bd62e3c387a0cec6417563e172b
benkoehlL <feynmandiagramateur@gmail.com>
2024-02-21 17:48:30 +0100

    finished main functionalities and calculation of outputs

--- LCOE.py
+++ LCOE.py
@@ -1,5 +1,5 @@
 import sys, os
-from cryptography.fernet import Fernet
+import numpy_financial as npf
 from PyQt6 import QtWidgets, uic
 from PyQt6.QtWidgets import QDialog, QFileDialog
 from PyQt6.QtGui import QIntValidator, QDoubleValidator
@@ -32,8 +32,8 @@ class Ui_LCOE(QDialog):
         
         self.pushButton_Calculate_KPIs.clicked.connect(self.KPIs)
 
-    def KPIs(self):
-        error_string = ''
+    def KPIs(self):        
+        ## get user input
         project_lifetime = int(self.input_project_lifetime.text())
         nominal_power = float(self.input_nominal_power.text())
         annual_yield = float(self.input_annual_yield.text())
@@ -46,6 +46,7 @@ class Ui_LCOE(QDialog):
         maintenance = 0.01*float(self.input_maintenance.text())
         inflation = 0.01*float(self.input_inflation.text())
         discount = 0.01*float(self.input_discount.text())
+        
         ## check whether all input fields are filled
         if False: pass#(project_lifetime == '' or
         #     nominal_power == '' or
@@ -59,10 +60,15 @@ class Ui_LCOE(QDialog):
         #     maintenance=='' or
         #     inflation == '' or
         #     discount == ''):
-        #    error_string += "You need to fill all input fields!\n"
-        #    self.display.setText(error_string)
+        #    display += "You need to fill all input fields!\n"
+        #    self.display.setText(display)
         else:
-            display = ''
+            
+            ## output LCOE
+            self.LCOE = 0.486848656921709
+            self.output_LCOE.setText(str(self.LCOE))
+            
+            ## calculate cash flow arrays 
             self.remaining_power = [1-(degradation*i) for i in range(project_lifetime)]
             self.energy_production = [nominal_power*annual_yield*self.remaining_power[i] for i in range(project_lifetime)]
             self.revenueFIT = [ self.energy_production[i]*(price*(1+index)**i) if price>0 else 0 for i in range(years)]
@@ -70,17 +76,52 @@ class Ui_LCOE(QDialog):
             self.maintenance = [-maintenance*TIP*(1+inflation)**i for i in range(project_lifetime)]
             self.earnings = [self.revenueFIT[i]+self.insurance[i]+self.maintenance[i] for i in range(project_lifetime)]
             self.income_cumul = [sum(self.earnings[0:i+1]) for i in range(project_lifetime)]
-            self.output_LCOE.setText(str(0.486848656921709))
-            self.LCOE = float(self.output_LCOE.text())
             self.income_LCOE = [self.LCOE*self.energy_production[i] for i in range(project_lifetime)]
             self.income_after_costs = [self.income_LCOE[i]+self.insurance[i]+self.maintenance[i] for i in range(project_lifetime)]
             self.discount_cash = [self.income_after_costs[i]/(1+discount)**(i+1) for i in range(project_lifetime)]
-            for i in range(project_lifetime):
-                display += f"after year {i+1}:\t remaining power: {round(self.remaining_power[i],3)}\t energy production: {round(self.energy_production[i],0)}\t revenue: {round(self.revenueFIT[i],2)}\t insurance: {round(self.insurance[i],2)}\t maintenance: {round(self.maintenance[i],2)}\t earnings: {round(self.earnings[i],2)}\t income cumul: {round(self.income_cumul[i],2)}\t income with LCOE: {round(self.income_LCOE[i],2)} \t income after costs: {round(self.income_after_costs[i],2)}\t discounted cash: {round(self.discount_cash[i],2)} \n"
             self.present_value = sum(self.discount_cash)
-            display += f"Present Value: {round(self.present_value,2)}"
-            self.display.setText(display)
             
+            ## output nominal power
+            self.nominal_power = nominal_power
+            self.output_nominal_power.setText(str(self.nominal_power))
+
+            ## output PVNI
+            if(discount==0.0):
+                 self.PVNI = sum(self.earnings)
+            else:
+                self.PVNI = self.XNPV(discount, [0] + self.earnings)
+                
+            self.output_PVNI.setText(str(self.PVNI))
+
+            ## output NPV
+            self.NPV = round(self.PVNI - TIP,2)
+            self.output_NPV.setText(str(self.NPV))
+
+            ## output IRR
+            self.IRR = round(npf.irr([TIP]+ self.earnings),2)
+            if(self.IRR <0):
+                self.IRR = 'N.A.'
+            self.output_ROI.setText(str(self.IRR))
+
+            ## display the cash flow calculations
+            display = self.create_cashflow_output(project_lifetime)
+            self.display.setText(display)
+
+    def XNPV(self, rate, *cashflow):
+        cashflow = cashflow[0]
+        return sum([cashflow[i]/(1+rate)**(i) for i in range(len(cashflow))])            
+
+    def create_cashflow_output(self, project_lifetime):
+        display_cash_output = ''
+        for i in range(project_lifetime):
+            display_cash_output += f"after year {i+1}:\t remaining power: {round(self.remaining_power[i],3)}\t energy production: {round(self.energy_production[i],0)}\t revenue: {round(self.revenueFIT[i],2)}\t insurance: {round(self.insurance[i],2)}\t maintenance: {round(self.maintenance[i],2)}\t earnings: {round(self.earnings[i],2)}\t income cumul: {round(self.income_cumul[i],2)}\t income with LCOE: {round(self.income_LCOE[i],2)} \t income after costs: {round(self.income_after_costs[i],2)}\t discounted cash: {round(self.discount_cash[i],2)} \n"
+        display_cash_output += f"Present Value: {round(self.present_value,2)}\n"
+        display_cash_output += f"PVNI: {round(self.PVNI,2)}\n"
+        display_cash_output += f"NPV: {round(self.NPV,2)}\n"
+        display_cash_output += f"Return on Investment: {self.IRR}"
+        
+        return display_cash_output
+
 app = QtWidgets.QApplication(sys.argv)
 window = Ui_LCOE()
 window.show()
